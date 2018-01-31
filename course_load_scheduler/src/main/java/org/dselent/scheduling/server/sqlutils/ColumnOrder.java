dabcde8a05d19049cc14ff70e4490cb52f01f176
MT-Grim <mtuckergrim@gmail.com>
2018-01-31 14:11:39 -0500

    ##Committed by Tucker. Trying to delete extra folder

--- course_load_scheduler/src/main/java/org/dselent/scheduling/server/sqlutils/ColumnOrder.java
+++
@@ -1,7 +0,0 @@
-package org.dselent.scheduling.server.sqlutils;
-
-public enum ColumnOrder
-{
-	ASC,
-	DESC
-}
--- course_load_scheduler/src/main/resources/sql/Inactive/PopulateTables.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_states(state) VALUES('ACTIVE');
-INSERT INTO user_states(state) VALUES('DEACTIVATED');
