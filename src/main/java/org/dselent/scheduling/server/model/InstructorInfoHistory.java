b8830a0ba44c80cb01dfeb871e073108ed718213
Leo Gonsalves <lcgonsalves@wpi.edu>
2018-02-02 18:38:29 -0500

    ## Commit by Leo
    Fixes:
        Fixed naming conventions for InstructorInfo classes and related classes and interfaces

--- src/main/java/org/dselent/scheduling/server/model/InstructorInfoHistory.java
+++ src/main/java/org/dselent/scheduling/server/model/InstructorInfoHistory.java
@@ -3,7 +3,7 @@ package org.dselent.scheduling.server.model;
 import java.sql.JDBCType;
 import java.util.*;
 
-public class Instructor_Info_History extends Model{
+public class InstructorInfoHistory extends Model{
 
     // table name
     public static final String TABLE_NAME = "instructor_info_history";
@@ -22,26 +22,26 @@ public class Instructor_Info_History extends Model{
     }
 
     // enum list
-    private static final List<Instructor_Info_History.Columns> COLUMN_LIST = new ArrayList<>();
+    private static final List<InstructorInfoHistory.Columns> COLUMN_LIST = new ArrayList<>();
 
     // type mapping
-    private static final Map<Instructor_Info_History.Columns, JDBCType> COLUMN_TYPE_MAP = new HashMap<>();
+    private static final Map<InstructorInfoHistory.Columns, JDBCType> COLUMN_TYPE_MAP = new HashMap<>();
 
     static
     {
-        for(Instructor_Info_History.Columns key : Instructor_Info_History.Columns.values())
+        for(InstructorInfoHistory.Columns key : InstructorInfoHistory.Columns.values())
         {
             COLUMN_LIST.add(key);
         }
 
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.ID, JDBCType.INTEGER);
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.INSTRUCTOR_INFO_ID, JDBCType.INTEGER);
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.RANK, JDBCType.VARCHAR);
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.COURSE_LOAD, JDBCType.INTEGER);
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.PHONE_NUMBER, JDBCType.VARCHAR);
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.OFFICE, JDBCType.VARCHAR);
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.USER_INFO_ID, JDBCType.INTEGER);
-        COLUMN_TYPE_MAP.put(Instructor_Info_History.Columns.DEPARTMENT, JDBCType.VARCHAR);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.ID, JDBCType.INTEGER);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.INSTRUCTOR_INFO_ID, JDBCType.INTEGER);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.RANK, JDBCType.VARCHAR);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.COURSE_LOAD, JDBCType.INTEGER);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.PHONE_NUMBER, JDBCType.VARCHAR);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.OFFICE, JDBCType.VARCHAR);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.USER_INFO_ID, JDBCType.INTEGER);
+        COLUMN_TYPE_MAP.put(InstructorInfoHistory.Columns.DEPARTMENT, JDBCType.VARCHAR);
     };
 
     // attributes
@@ -49,11 +49,21 @@ public class Instructor_Info_History extends Model{
     private Integer instructorInfoId;
     private String rank;
     private Integer courseLoad;
-    private String email;
     private String phoneNumber;
     private String office;
     private Integer userInfoId;
     private String department;
+
+    public static JDBCType getColumnType(InstructorInfoHistory.Columns column)
+    {
+        return COLUMN_TYPE_MAP.get(column);
+    }
+
+    public static String getColumnName(InstructorInfoHistory.Columns column)
+    {
+        return column.toString().toLowerCase();
+    }
+
 
     public static List<String> getColumnNameList()
     {
@@ -65,17 +75,6 @@ public class Instructor_Info_History extends Model{
         }
 
         return columnNameList;
-    }
-
-
-    public static JDBCType getColumnType(Columns column)
-    {
-        return COLUMN_TYPE_MAP.get(column);
-    }
-
-    public static String getColumnName(Columns column)
-    {
-        return column.toString().toLowerCase();
     }
 
     public static String getTableName() {
@@ -102,8 +101,8 @@ public class Instructor_Info_History extends Model{
         return instructorInfoId;
     }
 
-    public void setInstructorInfoId(Integer id) {
-        this.instructorInfoId = id;
+    public void setInstructorInfoId(Integer instructorInfoId) {
+        this.instructorInfoId = instructorInfoId;
     }
 
     public String getRank() {
@@ -122,20 +121,12 @@ public class Instructor_Info_History extends Model{
         this.courseLoad = courseLoad;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNuber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNuber(String phoneNuber) {
-        this.phoneNumber = phoneNuber;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public String getOffice() {
@@ -163,31 +154,14 @@ public class Instructor_Info_History extends Model{
     }
 
     @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Instructor_Info{");
-        sb.append("id=").append(id);
-        sb.append(", instructorInfoId='").append(instructorInfoId).append('\'');
-        sb.append(", rank='").append(rank).append('\'');
-        sb.append(", courseLoad=").append(courseLoad);
-        sb.append(", email='").append(email).append('\'');
-        sb.append(", phoneNuber='").append(phoneNumber).append('\'');
-        sb.append(", office='").append(office).append('\'');
-        sb.append(", userInfoId=").append(userInfoId);
-        sb.append(", department='").append(department).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Instructor_Info_History that = (Instructor_Info_History) o;
+        InstructorInfoHistory that = (InstructorInfoHistory) o;
         return Objects.equals(id, that.id) &&
                 Objects.equals(instructorInfoId, that.instructorInfoId) &&
                 Objects.equals(rank, that.rank) &&
                 Objects.equals(courseLoad, that.courseLoad) &&
-                Objects.equals(email, that.email) &&
                 Objects.equals(phoneNumber, that.phoneNumber) &&
                 Objects.equals(office, that.office) &&
                 Objects.equals(userInfoId, that.userInfoId) &&
@@ -197,6 +171,21 @@ public class Instructor_Info_History extends Model{
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, instructorInfoId, rank, courseLoad, email, phoneNumber, office, userInfoId, department);
+        return Objects.hash(id, instructorInfoId, rank, courseLoad, phoneNumber, office, userInfoId, department);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("InstructorInfoHistory{");
+        sb.append("id=").append(id);
+        sb.append(", instructorInfoId=").append(instructorInfoId);
+        sb.append(", rank='").append(rank).append('\'');
+        sb.append(", courseLoad=").append(courseLoad);
+        sb.append(", phoneNumber='").append(phoneNumber).append('\'');
+        sb.append(", office='").append(office).append('\'');
+        sb.append(", userInfoId=").append(userInfoId);
+        sb.append(", department='").append(department).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
