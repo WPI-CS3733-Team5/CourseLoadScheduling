728372e1272e921000ab5a1de7fc1123c8be6a8f
jacattelona <jacattelona@wpi.edu>
2018-02-02 19:02:15 -0500

    CalendarInfo underscores

--- src/main/java/org/dselent/scheduling/server/extractor/CalendarInfoExtractor.java
+++ src/main/java/org/dselent/scheduling/server/extractor/CalendarInfoExtractor.java
@@ -5,45 +5,45 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.dselent.scheduling.server.model.Calendar_Info;
+import org.dselent.scheduling.server.model.CalendarInfo;
 import org.dselent.scheduling.server.model.LabInfo;
 import org.dselent.scheduling.server.model.User_Info;
 import org.dselent.scheduling.server.model.UsersRolesLink;
 
-public class CalendarInfoExtractor extends Extractor<List<Calendar_Info>>{
+public class CalendarInfoExtractor extends Extractor<List<CalendarInfo>>{
 
     @Override
-    public List<Calendar_Info> extractData(ResultSet rs) throws SQLException {
-        List<Calendar_Info> resultList = new ArrayList<>();
+    public List<CalendarInfo> extractData(ResultSet rs) throws SQLException {
+        List<CalendarInfo> resultList = new ArrayList<>();
         while (rs.next()){
-            Calendar_Info result = new Calendar_Info();
+            CalendarInfo result = new CalendarInfo();
 
             //Set ID
-            result.setId(rs.getInt(Calendar_Info.getColumnName(Calendar_Info.Columns.ID)));
+            result.setId(rs.getInt(CalendarInfo.getColumnName(CalendarInfo.Columns.ID)));
             if (rs.wasNull()) result.setId(null);
 
             //Set Year
-            result.setYear(rs.getInt(Calendar_Info.getColumnName(Calendar_Info.Columns.YEAR)));
+            result.setYear(rs.getInt(CalendarInfo.getColumnName(CalendarInfo.Columns.YEAR)));
             if (rs.wasNull()) result.setYear(null);
 
             //Set Semester
-            result.setSemester(rs.getInt(Calendar_Info.getColumnName(Calendar_Info.Columns.SEMESTER)));
+            result.setSemester(rs.getInt(CalendarInfo.getColumnName(CalendarInfo.Columns.SEMESTER)));
             if (rs.wasNull()) result.setSemester(null);
 
             //Set Credit Amount
-            result.setCreditAmount(rs.getInt(Calendar_Info.getColumnName(Calendar_Info.Columns.CREDIT_AMOUNT)));
+            result.setCreditAmount(rs.getInt(CalendarInfo.getColumnName(CalendarInfo.Columns.CREDIT_AMOUNT)));
             if (rs.wasNull()) result.setCreditAmount(null);
 
             //Set Days
-            result.setDays(rs.getString(Calendar_Info.getColumnName(Calendar_Info.Columns.DAYS)));
+            result.setDays(rs.getString(CalendarInfo.getColumnName(CalendarInfo.Columns.DAYS)));
             if (rs.wasNull()) result.setDays(null);
 
             //Set StartTime
-            result.setStartTime(rs.getInt(Calendar_Info.getColumnName(Calendar_Info.Columns.START_TIME)));
+            result.setStartTime(rs.getInt(CalendarInfo.getColumnName(CalendarInfo.Columns.START_TIME)));
             if (rs.wasNull()) result.setStartTime(null);
 
             //Set EndTime
-            result.setEndTime(rs.getInt(Calendar_Info.getColumnName(Calendar_Info.Columns.END_TIME)));
+            result.setEndTime(rs.getInt(CalendarInfo.getColumnName(CalendarInfo.Columns.END_TIME)));
             if (rs.wasNull()) result.setEndTime(null);
 
             //add final result to list
